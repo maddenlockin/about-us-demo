def52f7cfd2c60da17eec09a918fdd6c45650edb
Pete Hamrick <pete.hamrick@gmail.com>
2022-03-02 12:39:07 -0800

    wrote renderPerson function

--- app.js
+++ app.js
@@ -11,7 +11,24 @@ const pronouns = document.querySelector('.pronouns');
 const favPlace = document.querySelector('.fav-place');
 const hobbies = document.querySelector('.hobbies');
 
-// set event listeners 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+// set event listeners
+
+selectElement.addEventListener('change', (event) => {
+    // call our render function here
+});
+
+function renderPerson(index) {
+    divContainer.classList.add(`${people[index].name}`);
+    name.textContent = 'Name: ' + people[index].name;
+    pronouns.textContent = 'Pronouns: ' + people[index].pronouns;
+    favPlace.textContent = 'Favorite Place: ' + people[index].favorite_place;
+
+    for (let hobby of people[index].hobbies) {
+        const li = document.createElement('li');
+        li.textContent = hobby;
+        hobbies.appendChild(li);
+    }
+}
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
